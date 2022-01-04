fa26fbffde0e42fb471252e2b1fb64250b5cb5ca
souz4s <p.uzah@outlook.com>
2022-01-04 10:05:07 -0300

    formatting the code

--- src/connection/access.js
+++ src/connection/access.js
@@ -5,12 +5,12 @@ const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const CODE = process.env.CODE;
 
-async function retunrJSON(){
+async function retunrJSON() {
     const response = await axios.post(`https://accounts.zoho.com/oauth/v2/token?client_id=${CLIENT_ID}&grant_type=authorization_code&client_secret=${CLIENT_SECRET}&redirect_uri=https://www.zylker.com/oauthredirect&code=${CODE}`);
 
     return response.data;
 }
 
 (async () => {
-    console.log(await retunrJSON())
-}) ();
+    console.log(await retunrJSON());
+})();
