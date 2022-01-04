0708ba01bfbbb9cd0871b9438cfaa0bfedd3678f
souz4s <p.uzah@outlook.com>
2022-01-04 16:52:24 -0300

    Refresh Token added

--- src/connection/access.js
+++ src/connection/access.js
@@ -5,12 +5,13 @@ const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const CODE = process.env.CODE;
 
-async function retunrJSON() {
-    const response = await axios.post(`https://accounts.zoho.com/oauth/v2/token?client_id=${CLIENT_ID}&grant_type=authorization_code&client_secret=${CLIENT_SECRET}&redirect_uri=https://www.zylker.com/oauthredirect&code=${CODE}`);
+async function access() {
+    const response1 = await axios.post(`https://accounts.zoho.com/oauth/v2/token?client_id=${CLIENT_ID}&grant_type=authorization_code&client_secret=${CLIENT_SECRET}&redirect_uri=https://www.zylker.com/oauthredirect&code=${CODE}`);
+    const refreshToken = response1.data.refresh_token;
+    // Refresh token
+    const response2 = await axios.post(`https://accounts.zoho.com/oauth/v2/token?refresh_token=${refreshToken}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=refresh_token`);
+    
+    console.log(response2.data);
+};
 
-    return response.data;
-}
-
-(async () => {
-    console.log(await retunrJSON());
-})();
+return access();
