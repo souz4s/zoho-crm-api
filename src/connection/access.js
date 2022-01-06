bcfcb174be676eeb7db34d09e5604cd72c8def22
souz4s <p.uzah@outlook.com>
2022-01-06 17:08:33 -0300

    export function withdrawn

--- src/connection/access.js
+++ src/connection/access.js
@@ -5,7 +5,7 @@ const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const CODE = process.env.CODE;
 
-export default async function access() {
+async function access() {
     const response = await axios.post(`https://accounts.zoho.com/oauth/v2/token?client_id=${CLIENT_ID}&grant_type=authorization_code&client_secret=${CLIENT_SECRET}&redirect_uri=https://www.zylker.com/oauthredirect&code=${CODE}`);
     const refreshToken = response.data.refresh_token;
 
