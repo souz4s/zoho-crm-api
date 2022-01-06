24e554d15abc4b06d4672fe11591673f79a6feda
souz4s <p.uzah@outlook.com>
2022-01-06 16:20:44 -0300

    exporting functions

--- src/connection/refresh.js
+++ src/connection/refresh.js
@@ -5,7 +5,7 @@ const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
 
-async function refresh() {
+export default async function refresh() {
     const response = await axios.post(`https://accounts.zoho.com/oauth/v2/token?refresh_token=${REFRESH_TOKEN}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=refresh_token`);
     const accessToken = response.data.access_token;   
 
