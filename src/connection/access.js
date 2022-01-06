3e967a94028211e5ba83d72b3f383b0771b9e187
souz4s <p.uzah@outlook.com>
2022-01-06 16:05:09 -0300

    separate token access and update functions

--- src/connection/access.js
+++ src/connection/access.js
@@ -6,12 +6,10 @@ const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const CODE = process.env.CODE;
 
 async function access() {
-    const response1 = await axios.post(`https://accounts.zoho.com/oauth/v2/token?client_id=${CLIENT_ID}&grant_type=authorization_code&client_secret=${CLIENT_SECRET}&redirect_uri=https://www.zylker.com/oauthredirect&code=${CODE}`);
-    const refreshToken = response1.data.refresh_token;
-    // Refresh token
-    const response2 = await axios.post(`https://accounts.zoho.com/oauth/v2/token?refresh_token=${refreshToken}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=refresh_token`);
-    
-    console.log(response2.data);
+    const response = await axios.post(`https://accounts.zoho.com/oauth/v2/token?client_id=${CLIENT_ID}&grant_type=authorization_code&client_secret=${CLIENT_SECRET}&redirect_uri=https://www.zylker.com/oauthredirect&code=${CODE}`);
+    const refreshToken = response.data.refresh_token;
+
+    console.log(refreshToken);
 };
 
 return access();
---
+++ src/connection/refresh.js
@@ -0,0 +1,15 @@
+const axios = require("axios");
+require("dotenv").config();
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
+
+async function refresh() {
+    const response = await axios.post(`https://accounts.zoho.com/oauth/v2/token?refresh_token=${REFRESH_TOKEN}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=refresh_token`);
+    const accessToken = response.data.access_token;   
+
+    console.log(accessToken);
+}
+
+return refresh();
